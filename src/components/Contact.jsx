1dd8fb7579062fbcfb14d67b6f595d4e049e485b
codex209 <221b220@juetguna.in>
2025-08-13 15:07:29 +0530

    updated my exp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Laxmikant",
+          to_name: "Laxmikant Shukla",
           from_email: form.email,
           to_email: "laxmikant.142003@gmail.com",
           message: form.message,
